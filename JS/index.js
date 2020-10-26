4eb28d766049bfa22a37234a524c3ddb202449cb
swsoman <64421666+swsoman@users.noreply.github.com>
2020-10-26 12:27:39 -0400

    added hover state to navgation

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function () {
+  $("li").hover(
+    function () {
+      $(this).css("color", "white");
+    },
+    function () {
+      $(this).css("color", "#303030");
+    }
+  );
+});
